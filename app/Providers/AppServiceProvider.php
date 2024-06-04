5410cbc021ff711ef028c96b9ce889ef687cb31e
yuzgec <olcayy@gmail.com>
2024-06-04 23:01:14 +0300

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         Carbon::setLocale(config('app.locale'));
-        //config()->set('settings', Setting::pluck('value','item')->all());
+        config()->set('settings', Setting::pluck('value','item')->all());
         URL::forceScheme('https');
 
         Blade::component('form-inputtext', InputText::class);
